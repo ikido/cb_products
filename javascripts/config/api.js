3504ef7b100ff91867483949c46b0f4697f983d9
Alexander Ponomarev <ikidoit@gmail.com>
2016-04-07 17:31:36 +0300

    fixed login issue

--- javascripts/config/api.js
+++ javascripts/config/api.js
@@ -3,7 +3,9 @@ import auth from 'lib/auth';
 
 API.config({
 	urlRoot: 'https://products.test.cb.bis.nl/api',
-	requestHeaders: { Authorization: `Bearer ${auth.token}` },
+	requestHeaders() {
+		return { Authorization: `Bearer ${auth.token}` }
+	},
   onRequestCompleted(response) {
     console.log('api', response.body);
   }
