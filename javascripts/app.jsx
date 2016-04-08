f8caf7e9902806222dff1dac88fab94a30c276e0
Alexander Ponomarev <ikidoit@gmail.com>
2016-04-08 15:42:34 +0300

    fixed filename error in app.jsx

--- javascripts/app.jsx
+++ javascripts/app.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import { render } from 'react-dom';
-import Routes from 'lib/Routes';
+import Routes from 'lib/routes';
 import 'config';
 
 import 'react-select/dist/react-select.css';
