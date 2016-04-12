cf13b8c9e9282e4da93628602ea98b192d03180c
Alexander Ponomarev <ikidoit@gmail.com>
2016-04-12 16:24:56 +0300

    file count for File attrubutes

--- javascripts/models/attribute_type.js
+++ javascripts/models/attribute_type.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { BaseModel } from 'mobx-model';
+import { autorun } from 'mobx';
 
 export default class AttributeType extends BaseModel {
 	
@@ -9,6 +10,21 @@ export default class AttributeType extends BaseModel {
 		valueType: '',
 		localized: false,
 		name: ''
-	}
+	};
+
+	// calculated list of attribute names to display dropzone for
+	static fileAttributes = [];
 
 }
+
+/*
+ * AttributeType.fileAttributes will be updated once any of the
+ * attribute types will be updated
+ */
+autorun(() => {
+	AttributeType.fileAttributes = AttributeType.all()
+		.slice()
+		.filter(a => a.valueType === 'File')
+		.map(a => a.name)
+})
+
--- javascripts/views/products/search/product_column.jsx
+++ javascripts/views/products/search/product_column.jsx
@@ -3,6 +3,9 @@
 import React, { PropTypes, Component } from 'react';
 import toString from 'lodash/toString';
 import result from 'lodash/result';
+import isArray from 'lodash/isArray';
+import includes from 'lodash/includes';
+import { AttributeType } from 'models';
 
 export default class ProductColumn extends Component {
   static propTypes = {
@@ -14,7 +17,16 @@ export default class ProductColumn extends Component {
   }
 
   render() {
+    //console.log('AttributeType.fileAttributes', AttributeType.fileAttributes)
+    let [attibutes, attrName] = this.props.column.path.split('.').slice(-2);
     let columnValue = result(this.props.product.toJSON(), this.props.column.path);
-    return <td>{ toString(columnValue) }</td>
+
+    if (!!columnValue && attibutes === 'attributes' && includes(AttributeType.fileAttributes, attrName)) {
+      columnValue = !!columnValue.slice ? columnValue.length : 1;
+    } else {
+      columnValue = toString(columnValue);
+    }
+    
+    return <td>{ columnValue }</td>
   }
 }
